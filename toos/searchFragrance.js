c678078c2f687d8d09a3d7d3691066049eac6b35
Rubenvill0811 <rubenvill0811@gmail.com>
2024-08-31 16:48:29 -0700

    added a search term encoder

--- toos/searchFragrance.js
+++ toos/searchFragrance.js
@@ -1,10 +1,12 @@
 const http = require("https");
-const searchTerm = "Gucci";
+const searchTerm = "Le Labo";
+const encodedTerm = encodeURIComponent(searchTerm);
+
 const options = {
   method: "GET",
   hostname: "fragrancefinder-api.p.rapidapi.com",
   port: null,
-  path: `/searchParfumes?perPage=3&page=1&keyword=${searchTerm}`,
+  path: `/searchParfumes?perPage=1&page=1&keyword=${encodedTerm}`,
   headers: {
     "x-rapidapi-key": "88c207fa04msh9ea402a879dd229p196e58jsn0fdac5525e50",
     "x-rapidapi-host": "fragrancefinder-api.p.rapidapi.com",
@@ -24,7 +26,7 @@ const req = http.request(options, function (res) {
     console.log(parsedData);
     const notesArray = parsedData.hits[0].notes;
     const mainAccords = parsedData.hits[0].main_accords;
-
+    // these loops show the data inside because of the notes and accords arrays.
     for (let i = 0; i < notesArray.length; i++) {
       console.log(notesArray[i]);
     }
